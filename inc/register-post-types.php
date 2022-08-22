ed534ce78c0954c616483bc36f8ad03df48a4eab
ErwiseDevelopment <erwisedev@gmail.com>
2022-08-22 01:15:21 -0300

    teste

--- inc/register-post-types.php
+++ inc/register-post-types.php
@@ -2,7 +2,7 @@
 
 function single_create_post_type() { 
 
-	register_post_type( 'post-example', array(
+	register_post_type( 'Galeria', array(
 		'labels'      => array( 'name' => 'Post Exemplo', 'singular_name' => 'Post Exemplo', 'all_items' => 'Todos os posts' ),
 		'public' 	  => true,
 		'has_archive' => true,
