89bf477d76dfb76f328701958f6e74a4414794a1
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-07 20:17:48 -0300

    teste1

--- template-parts/content-menu-editorials.php
+++ template-parts/content-menu-editorials.php
@@ -28,6 +28,24 @@
                                     href="#">
                                         institucional
                                     </a>
+                                    <a
+                                    class="w-100 u-box-shadow-pattern d-block u-font-size-14 u-font-weight-regular u-font-family-cinzel text-center text-uppercase text-decoration-none u-color-folk-white py-2"
+                                    style="background-color:#AF0101"
+                                    href="#">
+                                        Vocacional
+                                    </a>
+                                    <a
+                                    class="w-100 u-box-shadow-pattern d-block u-font-size-14 u-font-weight-regular u-font-family-cinzel text-center text-uppercase text-decoration-none u-color-folk-white py-2"
+                                    style="background-color:#AF0101"
+                                    href="#">
+                                        institucional
+                                    </a>
+                                    <a
+                                    class="w-100 u-box-shadow-pattern d-block u-font-size-14 u-font-weight-regular u-font-family-cinzel text-center text-uppercase text-decoration-none u-color-folk-white py-2"
+                                    style="background-color:#AF0101"
+                                    href="#">
+                                        institucional
+                                    </a>
                                 </div>
                             <?php } ?>
                             <!-- end loop -->
