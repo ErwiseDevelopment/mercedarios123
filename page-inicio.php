353ca3287210b97d7ec1c00807c3bdfbfb59105c
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-07 20:38:24 -0300

    teste1

--- page-inicio.php
+++ page-inicio.php
@@ -23,8 +23,8 @@ get_header(); ?>
 <?php echo get_template_part( 'template-parts/content', 'menu-editorials' ) ?>
 <!-- end menu -->
 
-<section class="w-100" style="height:100vh;background:red">
-[rev_slider alias="home"][/rev_slider]
+<section class="w-100" style="">
+<?php echo do_shortcode( '[rev_slider alias="home"][/rev_slider]' ); ?>
 </section>
 
 <!-- banner 
