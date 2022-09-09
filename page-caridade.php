c1b99e185d4e13a720559876666d267b865a7fff
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-09 12:23:46 -0300

    dinamico noticias caridade

--- page-caridade.php
+++ page-caridade.php
@@ -243,13 +243,14 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                         </div>
                                     </div>
                                 </a>
+                            </div>
                             
                                     <?php 
                                     endwhile;
                                     endif;
                                     wp_reset_query();
                                     ?>
-                            </div>
+                            
                             <!--  <php } ?>end slide -->
                     </div>
                 </div>
