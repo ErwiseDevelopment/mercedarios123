3b4ead95df721a3236739676edcc71c43e489033
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-09 15:37:24 -0300

    dinamico noticias caridade

--- template-parts/content-menu-editorials.php
+++ template-parts/content-menu-editorials.php
@@ -85,7 +85,7 @@
                                  </div>
                     
 
-                                <div class="col-xl-2 py-1 px-0 px-xl-3">
+                                <div class="col-xl-2 py-1">
                                     <a
                                     class="w-100 h-100 d-flex justify-content-center align-items-center u-font-size-14 u-font-weight-regular u-font-family-cinzel text-center text-uppercase text-decoration-none u-color-folk-white py-2"
                                     style="background-color:#AF0101"
