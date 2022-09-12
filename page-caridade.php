3756a8926a00ff9e2781b31cd5fb96b695e25fbb
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-11 22:57:27 -0300

    ajustes dinamico home

--- page-caridade.php
+++ page-caridade.php
@@ -572,7 +572,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
 
                             <!-- loop -->
                             <?php 
-                        $editorial_slug_current = 'caridade'
+                        $editorial_slug_current = 'caridade';
                         //strtolower(get_the_title());
 
                         $args = array(
