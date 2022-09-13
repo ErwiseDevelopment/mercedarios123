af13fbf30997587c728736a5e69f8452b893d5fc
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-13 08:44:11 -0300

    ajuste dinamico comunidade

--- page-educacao.php
+++ page-educacao.php
@@ -511,14 +511,11 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                              <?php
                                                 $post_categories = get_the_terms(get_the_ID(), 'comunidades-estados' );
                                                 $post_categories_current = array();
-                                                $term = 'rio-de-janeiro';
-                                                foreach( $post_categories as $post_category ) {
-                                                    foreach( $terms as $term ) {
-                                                        if( $post_category->name == $term->name )
+                                               
+                                              
                                                             array_push( $post_categories_current, $post_category->name );
-                                                    }
-                                                }
-                                                echo $post_categories_current[0];
+                                                                                                
+                                                echo $post_categories;
                                                 //echo var_dump($post_category, $post_categories_current);
                                             ?>
                                                 
