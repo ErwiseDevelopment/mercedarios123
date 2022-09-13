66e9e6d1308e004dfb0270f8d087e7994cf0330a
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-13 01:00:50 -0300

    adicionado videos

--- page-acervo-virtual.php
+++ page-acervo-virtual.php
@@ -52,7 +52,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                     <!-- loop -->
                     <?php 
                                 $args = array(
-                                    'posts_per_page' => 99,
+                                    'posts_per_page' => 1,
                                     'post_type'      => 'acervo');
 
                                     $contents = new WP_Query( $args );
