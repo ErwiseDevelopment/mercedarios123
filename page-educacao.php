3f5ee9854d05bf9de6ff4372926a6f6508afdce9
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-13 08:51:44 -0300

    ajuste dinamico comunidade

--- page-educacao.php
+++ page-educacao.php
@@ -518,8 +518,8 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                                             array_push( $post_categories_current, $post_category->name );
                                                     
                                                 }
-                                               // echo $post_categories_current[0];
-                                                echo var_dump( $post_category);
+                                               echo $post_category;
+                                                //echo var_dump( $post_category);
                                             ?>
                                                 
                                             </p>
