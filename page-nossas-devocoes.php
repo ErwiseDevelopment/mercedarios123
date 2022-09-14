269881bee07a84a002d3122fa5903452ddcd2756
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-14 00:55:18 -0300

    ajustes comunidades

--- page-nossas-devocoes.php
+++ page-nossas-devocoes.php
@@ -104,49 +104,52 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                     <span class="l-template-content__content d-block u-font-family-lato">
                                         <!-- //<php the_content() ?> -->
                                     </span>
-                </div>       </div>             
-            </div>
-            <div class="col-12">
+                                 </div>
+                            </div>             
+                      </div>
+                            <div class="col-12">
 
-                <div class="row">
+                                <div class="row">
 
-                    <?php
-                        $args = array(
-                            'posts_per_page' => -1,
-                            'post_type'      => $post->post_type,
-                            'order'          => 'DESC',
-                            
-                            'tax_query'      => array(
-                                array(
-                                    'taxonomy' => 'santos',
-                                    'field'    => 'slug',
-                                    'terms'    => array( $single_category->slug )
-                                )
-                            )
-                        );
+                                    <?php
+                                        $args = array(
+                                            'posts_per_page' => -1,
+                                            'post_type'      => $post->post_type,
+                                            'order'          => 'DESC',
+                                            
+                                            'tax_query'      => array(
+                                                array(
+                                                    'taxonomy' => 'santos',
+                                                    'field'    => 'slug',
+                                                    'terms'    => array( $single_category->slug )
+                                                )
+                                            )
+                                        );
 
-                        $other_posts = new WP_Query( $args );
+                                        $other_posts = new WP_Query( $args );
 
-                        if( $other_posts->have_posts() ) :
-                            while( $other_posts->have_posts() ) : $other_posts->the_post();
-                    ?>
-                                <div class="col-lg-4 my-2">
-                                    <a href="<?php the_permalink() ?>">
-                                        <img
-                                        class="img-fluid w-100 u-object-fit-cover"
-                                        style="height:234px"
-                                        src="<?php echo get_field( 'imagem' ) ?>"
-                                        alt="<?php the_title() ?>">
-                                    </a>
-                                </div>
-                    <?php
-                            endwhile;
-                        endif;
+                                        if( $other_posts->have_posts() ) :
+                                            while( $other_posts->have_posts() ) : $other_posts->the_post();
+                                    ?>
+                                                <div class="col-lg-4 my-2">
+                                                    <a href="<?php the_permalink() ?>">
+                                                        <img
+                                                        class="img-fluid w-100 u-object-fit-cover"
+                                                        style="height:234px"
+                                                        src="<?php echo get_field( 'imagem' ) ?>"
+                                                        alt="<?php the_title() ?>">
+                                                    </a>
+                                                </div>
+                                    <?php
+                                            endwhile;
+                                        endif;
 
-                        wp_reset_query();
-                    ?>
-                </div>  
-            </div>
+                                        wp_reset_query();
+                                    ?>
+                                </div>  
+                            </div>      
+                </div>
+            </div>               
         </div>
     </div>
 </section>
