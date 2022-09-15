c2e9272edafdac98d0464c60f72c490f1e920391
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-15 01:36:35 -0300

    correcao noticias

--- single-post.php
+++ single-post.php
@@ -72,8 +72,8 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
 									$count++;
 							?>
 									<span class="u-font-weight-regular u-color-folk-dark-golden">
-										<?php echo $cat->name; ?>	
-										<?php echo var_dump($cats)?>
+										<?php echo $cats[1]; ?>	
+										
 									</span>
 							<?php 
 									if( $count == 3 )
