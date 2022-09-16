97449e5e6cd655431e39f9af787a3de20f8d1999
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-16 16:12:06 -0300

    page blog

--- page-governo-provincial.php
+++ page-governo-provincial.php
@@ -201,7 +201,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                         </div>
 
                                          <div class="card-body pl-0">
-                                                        <div class="l-areas__box  d-flex flex-column justify-content-center align-items-center u-bg-folk-dark-marron py-4 px-5">      
+                                                        <div class="l-areas__box  d-flex flex-column justify-content-center align-items-center u-bg-folk-dark-marron py-2 px-3">      
                                                             <h5 class=" l-areas__box__title u-font-size-25 u-font-weight-regular u-font-family-cinzel text-center u-color-folk-white ">
                                                                 <!-- Padre Paulo Manoel de Souza Profilo -->
                                                                 <?php the_title() ?>
