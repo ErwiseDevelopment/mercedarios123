7335a0d7ab1b64b489910f195b57b28146af2352
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-17 21:10:21 -0300

    page devovoes

--- page-materiais.php
+++ page-materiais.php
@@ -244,9 +244,10 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                                     <a href="<?php echo get_field('link_ebook') ?>" 
                                                     <?php if('nova_guia_mat' == '1'):?> target="_blank"
                                                     <?php endif;?>>
-                                                    <p class="u-font-size-12 u-font-weight-bold u-font-family-nunito text-center u-color-folk-white u-bg-folk-bold-marron hover:u-bg-folk-dark-golden mb-0 py-2">
-                                                        Baixar
-                                                    </p></a>
+                                                        <p class="u-font-size-12 u-font-weight-bold u-font-family-nunito text-center u-color-folk-white u-bg-folk-bold-marron hover:u-bg-folk-dark-golden mb-0 py-2">
+                                                            Baixar
+                                                        </p>
+                                                    </a>
                                                 </div>
                                             </div>
                                             
