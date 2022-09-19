251c8941c18487a92bcd68746ae859d8cc13e667
Fabio de Melo <fabiomessias98@outlook.com>
2022-09-19 15:08:49 -0300

    Ajustado o dinamico da pagina Nossas Devocoes com filtro de categorias

--- page-nossas-devocoes.php
+++ page-nossas-devocoes.php
@@ -51,7 +51,6 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
 
                     <div class="col-6 pt-3">
 
-                        
                         <ul class="d-flex pl-0">
 
                             <li class="u-list-style-none mr-1">
@@ -77,86 +76,141 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                     > Nossas Devoções
                                 </a>
                             </li>
-
-                            <!-- <li class="u-list-style-none mr-1">
-                                <a
-                                class="u-font-size-15 u-font-weight-bold u-font-family-lato u-color-folk-bold-gray"
-                                href="<php echo $category_current ? get_home_url( null, 'nossas-devocoes/?id=' . $category_current->term_id ) : '#'; ?>">
-                                    Santos da Ordem > 
-                                    <php echo '> ' . $category_current->name; ?>
-                                </a>
-                            </li> -->
                         </ul>
-                        <!-- <h2 class="u-font-size-34 u-font-weight-bold u-font-family-cinzel u-color-folk-bold-marron mb-3">
-                            
-                            <php echo the_title() ?>
-                        </h2> -->
-                    </div>
-                       <!-- content -->
-                        <section class="mt-3 py-5">
-
-                        <div class="container">
-
-                            <div class="row justify-content-center">
-
-                                <div class="col-12">
-
-                                    <span class="l-template-content__content d-block u-font-family-lato">
-                                       <?php the_content() ?>
-                                    </span>
-                                 </div>
-                            </div>             
-                      </div>
-                            <div class="col-12">
-
-                                <div class="row">
-
-                                    <?php
-                                        $args = array(
-                                            'posts_per_page' => -1,
-                                            'post_type'      => 'devocoes',
-                                            'order'          => 'DESC',
-                                            
-                                            'tax_query'      => array(
-                                                array(
-                                                    'taxonomy' => 'santos',
-                                                    'field'    => 'slug',
-                                                    'terms'    => array( $single_category->slug )
-                                                )
-                                            )
-                                        );
-                                          
-                                        $other_posts = new WP_Query( $args );
-                                        $category = get_the_terms( get_the_category(), 'santos' );
-                                        
-                                        if( $other_posts->have_posts() ) :
-                                            while( $other_posts->have_posts() ) : $other_posts->the_post();
-                                    ?>
-                                                <div class="col-lg-4 my-2">
-                                                    <a href="<?php the_permalink() ?>">
-                                                        <img
-                                                        class="img-fluid w-100 u-object-fit-cover"
-                                                        style="height:234px"
-                                                        src="<?php echo get_field( 'imagem' ) ?>"
-                                                        alt="<?php the_title() ?>">
-                                                        
-                                                    </a>
-                                                </div>
-                                    <?php
-                                            endwhile;
-                                        endif;
-
-                                        wp_reset_query();
-                                    ?>
-                                </div>  
-                            </div>      
+                    </div>    
                 </div>
-            </div>               
+            </div>    
+            
+            <div class="col-12">
+
+                <div class="row">
+
+                    <!-- loop -->
+                    <?php 
+                        if( isset( $_GET[ 'id' ]) ) :
+                            $category_current_id = $_GET[ 'id' ];
+                            $category_current = get_term( $category_current_id, 'santos' );
+
+                            $args = array(
+                                'posts_per_page' => -1,
+                                'post_type'      => 'devocoes',
+                                'order'          => 'DESC',
+                                'tax_query'      => array(
+                                    array(
+                                        'taxonomy' => 'santos',
+                                        'field'    => 'slug',
+                                        'terms'    => array( $category_current->slug )
+                                    )
+                                )
+                            );
+
+                            $posts_devotions = new WP_Query( $args );
+                            
+                            if( $posts_devotions->have_posts() ) :
+                                while( $posts_devotions->have_posts() ) : $posts_devotions->the_post();
+                    ?>
+                                    <div class="col-lg-4 my-2">
+                                        <a href="<?php the_permalink() ?>">
+                                            <img
+                                            class="img-fluid w-100 u-object-fit-cover"
+                                            style="height:234px"
+                                            src="<?php echo get_field( 'imagem' ) ?>"
+                                            alt="<?php the_title() ?>">
+                                        </a>
+                                    </div>
+                    <?php
+                                endwhile;
+                            endif;
+                        else :
+                            $all_categories = get_terms( array(
+                                'taxonomy' => 'santos',
+                                'hide_empty' => false,
+                            ));
+
+                            foreach( $all_categories as $category ) :
+                    ?>
+                                <div class="col-lg-4 my-2">
+                                    <a href="<?php echo get_home_url( null, 'nossas-devocoes/?id=' . $category->term_id ); ?>">
+                                        <img
+                                        class="img-fluid w-100 u-object-fit-cover"
+                                        style="height:234px"
+                                        src="<?php echo get_field( 'imagem_dev', $category ) ?>"
+                                        alt="">
+                                    </a>
+                                </div>
+                    <?php
+                            endforeach;
+                        endif;
+                    ?>
+                    <!-- end loop -->
+                </div>
+            </div>
         </div>
     </div>
 </section>
-                        <!-- end content -->
+<!-- end content -->
                     
+<!-- other posts -->
+<section class="mt-3 py-5 d-none">
+
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <div class="col-12">
+
+                <span class="l-template-content__content d-block u-font-family-lato">
+                    <?php the_content() ?>
+                </span>
+            </div>
+
+            <div class="col-12">
+
+                <div class="row">
+
+                    <?php
+                        $args = array(
+                            'posts_per_page' => -1,
+                            'post_type'      => 'devocoes',
+                            'order'          => 'DESC',
+                            
+                            'tax_query'      => array(
+                                array(
+                                    'taxonomy' => 'santos',
+                                    'field'    => 'slug',
+                                    'terms'    => array( $single_category->slug )
+                                )
+                            )
+                        );
+                        
+                        $other_posts = new WP_Query( $args );
+                        $category = get_the_terms( get_the_category(), 'santos' );
+                        
+                        if( $other_posts->have_posts() ) :
+                            while( $other_posts->have_posts() ) : $other_posts->the_post();
+                    ?>
+                                <div class="col-lg-4 my-2">
+                                    <a href="<?php the_permalink() ?>">
+                                        <img
+                                        class="img-fluid w-100 u-object-fit-cover"
+                                        style="height:234px"
+                                        src="<?php echo get_field( 'imagem' ) ?>"
+                                        alt="<?php the_title() ?>">
+                                        
+                                    </a>
+                                </div>
+                    <?php
+                            endwhile;
+                        endif;
+
+                        wp_reset_query();
+                    ?>
+                </div>  
+            </div>   
+        </div>
+    </div>
+</section>
+<!-- end other posts -->
 
 <?php endwhile; ?>
 
