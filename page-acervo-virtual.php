d65de6a3573dae876332b7471ef010e6e0fc0174
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-19 13:21:33 -0300

    ajuste acervo

--- page-acervo-virtual.php
+++ page-acervo-virtual.php
@@ -66,10 +66,9 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
 
                                     <a 
                                     class="h-100 position-relative d-block text-decoration-none  pt-5 pb-5 pb-lg-4 px-4" 
-                                    <?php if (empty (the_post_thumbnail('post-thumbnail'))):
-                                    { ?> class="u-bg-folk-dark-marron"  <?php } else:  the_post_thumbnail('post-thumbnail'); 
-                                endif;?>         
-                       href="<?php echo get_field( 'acessar' ) ?>"
+                                    <?php the_post_thumbnail('post-thumbnail'); 
+                                      ?>         
+                                     href="<?php echo get_field( 'acessar' ) ?>"
                                     target="<?php echo get_field( 'nova_guia' ) ?>"
                                     rel="noreferrer noopener">
                                         <span class="l-collection__icon-download">
