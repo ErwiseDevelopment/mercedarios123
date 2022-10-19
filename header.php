615ba99db26bf86acb3a2fdaef5cb86ba9df7caf
Fabio Leandro <fabiomessias98@outlook.com>
2022-10-19 17:40:39 -0300

    Finalizado pagina de pesquisa

--- header.php
+++ header.php
@@ -96,13 +96,13 @@
 
                     <div class="row justify-content-center">
 
-                        <div class="col-8">
+                        <div class="col-lg-8">
 
                             <form method="GET" action="<?php echo get_home_url( null, '/' ) ?>">
 
                                 <div class="row justify-content-end">
 
-                                    <div class="col-8">
+                                    <div class="col-lg-8 mb-3 mb-lg-0">
                                         <input 
                                         class="w-100 border shadow-sm rounded-lg d-block u-font-weight-regular px-3"
                                         type="search"
@@ -110,14 +110,14 @@
                                         placeholder="...">
                                     </div>
 
-                                    <div class="col-3 pl-0">
+                                    <div class="col-lg-3 mb-3 mb-lg-0 pl-lg-0">
                                         <input
                                         class="w-100 h-100 border-0 shadow-sm rounded-lg d-block u-font-size-12 u-font-weight-bold text-center text-uppercase u-color-folk-white u-bg-folk-golden" 
                                         type="submit"
                                         value="Pesquisar">
                                     </div>
 
-                                    <div class="col-1 d-flex justify-content-center align-items-center pl-0">
+                                    <div class="col-lg-1 d-flex justify-content-center align-items-center pl-lg-0">
                                         <span class="w-100 rounded-lg u-font-weight-bold text-center u-color-folk-white u-bg-folk-dark-marron u-cursor-pointer js-close-search">
                                             x
                                         </span>
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,22 +9,68 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+<!-- <article class="col-12" id="post-<?php the_ID(); ?>" <?php post_class(); ?>> -->
+<a 
+class="col-12 d-block text-decoration-none my-4"
+href="<?php the_permalink() ?>">
 
-		<?php if ( 'post' === get_post_type() ) : ?>
+	<div class="row">
+
+		<div class="col-md-5 pr-md-0">
+			<?php
+				$alt_title = get_the_title();
+
+				the_post_thumbnail( 'post-thumbnail',
+					array(
+						'class' => 'img-fluid w-100 h-100 u-object-fit-cover',
+						'alt'   => $alt_title
+					));
+			?>
+		</div>
+
+		<div class="col-md-7 pl-md-0">
+			
+			<div class="l-template-blog__box py-4 px-3 px-md-5">
+
+				<p class="u-font-size-12 xxl:u-font-size-15 u-font-weight-bold text-uppercase u-color-folk-golden mb-0">
+					<!-- 10 de dezembro de 2021 -->
+					<?php echo get_the_date( 'd/m/Y', $post->ID ) ?>
+				</p>
+
+				<h4 class="l-template-blog__title u-font-weight-bold u-color-folk-dark-marron">
+					<?php the_title() ?>
+				</h4>
+
+				<span class="d-block u-font-size-14 xxl:u-font-size-18 u-font-weight-regular u-color-folk-gray pb-3">
+					<?php the_excerpt(); ?>
+				</span>
+
+				<div class="row">
+
+					<div class="col-6 col-lg-4">
+						<p class="w-100 d-block u-font-weight-medium text-center text-decoration-none u-color-folk-white u-bg-folk-golden hover:u-bg-folk-dark-marron mb-0 p-3">
+							Ler mais
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</a>
+	<!-- <header class="entry-header">
+		<php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+		<php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php wp_bootstrap_starter_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+			<php wp_bootstrap_starter_posted_on(); ?>
+		</div>
+		<php endif; ?>
+	</header>
 
 	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
+		<php the_excerpt(); ?>
+	</div>
 	<footer class="entry-footer">
-		<?php wp_bootstrap_starter_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+		<php wp_bootstrap_starter_entry_footer(); ?>
+	</footer> -->
+<!-- </article> -->
