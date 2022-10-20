53899a9a793cdba96c9dfd7d80fcc97ea21444a5
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-20 09:17:57 -0300

    dinamico sportify

--- footer.php
+++ footer.php
@@ -268,6 +268,49 @@
                         </ul>
                 </div>
             </div>
+            					<!-- footer logos -->
+                                <div class="col-12 my-4">
+                        
+                        <div class="row">
+
+                            <div class="col-lg-8 d-flex align-items-center">
+                                <p class="u-line-height-100 u-font-weight-semibold text-center text-md-left u-color-folk-white mb-0" style= "font-size: 13px;">
+                                    ORDEM MERCEDÁRIA © <?php echo date('Y'); ?> TODOS OS DIREITOS RESERVADOS.
+                                </p>
+                            </div>
+
+                            <div class="col-lg-4">
+                            
+                                <div class="row">
+
+                                    <div class="col-6">
+                                        <a 
+                                        href="https://www.dominuscomunicacao.com/" 
+                                        target="_blank" 
+                                        rel="noreferrer noopener">
+                                            <img
+                                            class="img-fluid my-3 my-md-0"
+                                            src="https://erwise.com.br/wp-content/uploads/2022/06/dominus.png"
+                                            alt="Dominus">
+                                        </a>
+                                    </div>
+
+                                    <div class="col-6">
+                                        <a 
+                                        href="https://www.erwise.com.br" 
+                                        target="_blank" 
+                                        rel="noreferrer noopener">
+                                            <img
+                                            class="img-fluid my-3 my-md-0"
+                                            src="https://erwise.com.br/wp-content/uploads/2022/06/erwise.png"
+                                            alt="Erwise">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+					<!-- end footer logo -->
         </div>
     </footer>
 
