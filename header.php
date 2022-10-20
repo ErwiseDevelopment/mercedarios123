9113387871d9d01743647a961d08a0e170f1e98a
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-20 01:48:33 -0300

    dinamico sportify

--- header.php
+++ header.php
@@ -22,10 +22,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-<?php wp_head(); ?>
+<!-- <php wp_head(); ?> -->
 </head>
 
-<body 
+<body <?php body_class(); ?>>
 
 <?php 
     // WordPress 5.2 wp_body_open implementation
