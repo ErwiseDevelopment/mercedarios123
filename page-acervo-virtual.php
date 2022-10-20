38421dcf9cb78580d128d99b3257e7d4178aa69b
Fabio Leandro <fabiomessias98@outlook.com>
2022-10-20 00:59:45 -0300

    Ajustado o icone da pagina acervo

--- page-acervo-virtual.php
+++ page-acervo-virtual.php
@@ -73,7 +73,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                         <?php if( get_field( 'img_destaque_acervo' ) ) : ?>
                                             <span class="l-collection__icon-download">
                                                 <img
-                                                class="img-fluid"
+                                                class="img-fluid w-100 u-object-fit-cover"
                                                 src="<?php echo get_field( 'img_destaque_acervo' ) ?>"
                                                 alt="Ícone download">
                                             </span>
@@ -85,7 +85,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                                 alt="Ícone download">
                                             </span>
                                         <?php endif; ?>
-                                        
+
                                         <h4 class="u-font-size-18 xl:u-font-size-22 xxl:u-font-size-32 u-font-weight-regular u-font-family-lato text-center u-color-folk-white mt-3">
                                             
 											 <?php $texto = get_field( 'descricao' ); 
