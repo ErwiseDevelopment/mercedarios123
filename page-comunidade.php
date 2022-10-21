44241f1bd3c72f58e63213013d2716650c1363d2
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-21 10:23:06 -0300

    ajustes finais

--- page-comunidade.php
+++ page-comunidade.php
@@ -209,7 +209,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                  array(
                                     'taxonomy' => 'comunidades-estados',
                                     'hide_empty' => false,
-                                    'order' => 'DSC'
+                                    'order' => [29,31,30,26,81]
                                 ));
                             
                             //$order = array($term->term_id);
