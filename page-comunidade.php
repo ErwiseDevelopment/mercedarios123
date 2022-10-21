71323a8ae01c0f2556bf768b23c1220fdaa80f5e
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-21 09:32:15 -0300

    ajustes finais

--- page-comunidade.php
+++ page-comunidade.php
@@ -208,7 +208,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                             $terms = get_terms( 'comunidades-estados', array(
                                 'hide_empty' => false,
                                   'parent' => 0,
-                                  'term_ids' => ['29','31','30','26','81']
+                                  'term_id' => [29,31,30,26,81]
                             ));
                             //$order = array($term->term_id);
                             foreach( $terms as $term )  :
@@ -223,7 +223,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                     class="w-100 d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-regular u-font-family-lato text-center text-decoration-none u-color-folk-white u-bg-folk-dark-marron hover:u-bg-folk-dark-golden py-2" 
                                     href="<?php echo get_home_url( null, 'comunidade/?cat=' . $term->slug ); ?>">
                                         <!-- Institucional -->
-                                        <?php var_dump( $term); ?>
+                                        <?php echo $term->name; ?>
                                     </a>
                                 </div>
                         <?php  endforeach;    ?>
