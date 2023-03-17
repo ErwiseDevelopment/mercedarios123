87d477e3103e96f89e12885e7af1eb0a4d738d8c
ErwiseDevelopment <erwise.dev@gmail.com>
2023-03-17 15:51:19 -0300

    teste carregar mais post

--- page-blog.php
+++ page-blog.php
@@ -245,7 +245,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
 
                     <?php 
                         $args = array(
-                            'posts_per_page' => 6,
+                            'posts_per_page' => 3,
                             'post_type'      => 'post',
                             'category_name'  => $category_current . ',+blog',
                             'order'          => 'DSC',
