ad2a0a2b167afa22638c454133dec400c73939c7
ErwiseDevelopment <erwise.dev@gmail.com>
2023-03-17 16:49:11 -0300

    teste carregar mais post

--- page-blog.php
+++ page-blog.php
@@ -340,12 +340,6 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
 <?php endwhile; ?>
 <script>
 
-$(document).ready(function() {
-  $('.load-more-posts-container button').on('click', function(e) {
-    e.preventDefault();
-    loadPosts();
-  });
-});
   function loadPosts(){
     if(!loading){
         loading = true;
