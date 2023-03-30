bb25183522f2bd80be96903034dd6cf340f9dfa5
ErwiseDevelopment <erwise.dev@gmail.com>
2023-03-29 23:51:12 -0300

    ajuste regra de não exibir fotos se não tiver

--- page-paroquias.php
+++ page-paroquias.php
@@ -300,7 +300,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
                                             $contents = new WP_Query( $args );
                                             $cats = array();
                                          $poste = $contents->have_posts();
-                                    if( $poste = 1) :
+                                         var_dump($poste);
                                     ?>
 <section class="py-5">
            
@@ -433,7 +433,7 @@ style="background-image: url(<?php echo get_template_directory_uri()?>/../wp-boo
 </section>
 
 <!-- end blog -->
-<?php endif; ?>
+<?php  ?>
 <!-- our construction -->
 <section class="py-5">
 
